2e9416e2fad894ee034f5bca95c1e1171d7452b7
kohanov <alexkoh192spb@mail.ru>
2016-10-27 00:17:56 +0300

    drawCircle

--- src/main/java/fractal/MainFrame.java
+++ src/main/java/fractal/MainFrame.java
@@ -7,10 +7,11 @@ import de.erichseifert.gral.plots.lines.LineRenderer;
 import de.erichseifert.gral.plots.points.PointRenderer;
 import de.erichseifert.gral.ui.InteractivePanel;
 
+import org.apache.commons.math3.complex.Complex;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
 
 /**
  * Main frame for fractal task
@@ -18,41 +19,81 @@ import java.awt.event.MouseEvent;
  * @author Danil Kolikov
  */
 public class MainFrame extends JFrame {
+    private static final Color red = new Color(1.0f, 0.0f, 0.0f);
+    private static final Color green = new Color(0.0f, 1.0f, 0.0f);
+    private static final Color blue = new Color(0.0f, 0.0f, 1.0f);
+
+    private DataTable firstRootData = new DataTable(Double.class, Double.class);
+    private DataTable secondRootData = new DataTable(Double.class, Double.class);
+    private DataTable thirdRootData = new DataTable(Double.class, Double.class);
+    private XYPlot plot = new XYPlot();
+    private InteractivePanel interactivePanel;
+
+    LineRenderer lineRenderer = new DefaultLineRenderer2D();
+
+    private void drawCircle() {
+        DataTable circleData = new DataTable(Double.class, Double.class);
+        for (int i = 0; i <= 720; i++) {
+            double x = Math.cos(i * Math.PI / 360);
+            double y = Math.sin(i * Math.PI / 360);
+            circleData.add(x, y);
+        }
+        plot.add(circleData);
+        plot.setLineRenderers(circleData, lineRenderer);
+        for (PointRenderer pR : plot.getPointRenderers(circleData)) {
+            Shape circle = new Ellipse2D.Double(-4.0, -4.0, 8.0, 8.0);
+            pR.setShape(circle);
+        }
+    }
+
+    private void drawPoint(Complex point, int pointColor) {
+        switch (pointColor) {
+            case 0:
+                firstRootData.add(point.getReal(), point.getImaginary());
+                for (PointRenderer pR : plot.getPointRenderers(firstRootData)) {
+                    Shape circle = new Ellipse2D.Double(-4.0, -4.0, 8.0, 8.0);
+                    pR.setShape(circle);
+                    pR.setColor(red);
+                }
+                break;
+            case 1:
+                secondRootData.add(point.getReal(), point.getImaginary());
+                for (PointRenderer pR : plot.getPointRenderers(secondRootData)) {
+                    Shape circle = new Ellipse2D.Double(-4.0, -4.0, 8.0, 8.0);
+                    pR.setShape(circle);
+                    pR.setColor(green);
+                }
+                break;
+            case 2:
+                thirdRootData.add(point.getReal(), point.getImaginary());
+                for (PointRenderer pR : plot.getPointRenderers(thirdRootData)) {
+                    Shape circle = new Ellipse2D.Double(-4.0, -4.0, 8.0, 8.0);
+                    pR.setShape(circle);
+                    pR.setColor(blue);
+                }
+                break;
+        }
+    }
+
     public MainFrame() throws HeadlessException {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new Dimension(600, 400));
+        setMinimumSize(new Dimension(600, 600));
 
-        DataTable data = new DataTable(Double.class, Double.class);
-        XYPlot plot = new XYPlot();
-        LineRenderer lines = new DefaultLineRenderer2D();
+        drawCircle();
+        plot.add(firstRootData);
+        plot.add(secondRootData);
+        plot.add(thirdRootData);
 
-        // count data to cos(x)
-        for (double x = -5.0; x <= 5.0; x += 0.25) {
-            double y = Math.cos(x);
-            data.add(x, y);
-        }
-        plot.add(data);
-
-        // set lines
-        plot.setLineRenderers(data, lines);
-
-        // set colors
-        Color color = new Color(0.0f, 0.0f, 1.0f);
-        Color red = new Color(1.0f, 0.0f, 0.0f);
-        for (PointRenderer pR : plot.getPointRenderers(data)) {
-            pR.setColor(red);
-        }
-        for (LineRenderer lR : plot.getLineRenderers(data)) {
-            lR.setColor(color);
-        }
-
-        InteractivePanel interactivePanel = new InteractivePanel(plot);
+        interactivePanel = new InteractivePanel(plot);
         getContentPane().add(interactivePanel);
     }
 
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
         mainFrame.setTitle("Fractals");
+        mainFrame.drawPoint(new Complex(0.0f, 0.0f), 0);
+        mainFrame.drawPoint(new Complex(0.5f, 0.5f), 1);
+        mainFrame.drawPoint(new Complex(1.5f, 1.5f), 2);
         mainFrame.setVisible(true);
     }
 }
