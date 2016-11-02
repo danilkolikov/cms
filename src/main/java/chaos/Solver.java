5e0d91b3da5af1e4ba7102dc44d308b248ef06ca
danilkolikov <danilkolikov@gmail.com>
2016-11-02 21:14:29 +0300

    Totally new solution for chaos task
    - Draws correct bifurcation diagram
    - Correctly detects chaos

--- src/main/java/chaos/Solver.java
+++ src/main/java/chaos/Solver.java
@@ -1,11 +1,12 @@
 package chaos;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.lang.Math.abs;
+import static java.util.Collections.emptyList;
 
 /**
  * Solver for chaos task
@@ -25,33 +26,6 @@ public class Solver {
     }
 
     /**
-     * Find one root of x = r * x * (1 - x)
-     *
-     * @param r             Parameter
-     * @param eps           Epsilon
-     * @param maxIterations Maximal number of iterations
-     * @return Found root, if method converges, or find one of roots, if method doesn't converge
-     */
-    public static double findRoot(double r, double eps, int maxIterations) {
-        // phi' = r * (1 - 2 * x)
-        // For converge, we need that |phi'(x)| <= q < 1
-        // Assume that we need q = 1/2
-        // Then we should take such x that (2 * r - 1) / (4 * r) < x < (2 * r + 1) / (4 * r)
-        // Let's take the middle
-        double current = 0.5;
-        double previous;
-        int iterations = 0;
-        do {
-            previous = current;
-            current = f(r, previous);
-            if (abs(current - previous) < eps) {
-                break;
-            }
-        } while (++iterations < maxIterations);
-        return current;
-    }
-
-    /**
      * Find all roots of x = r * x * (1 - x) to which method of simple iteration converges
      *
      * @param r   Parameter
@@ -60,83 +34,101 @@ public class Solver {
      */
     @Nonnull
     public static List<Double> findRoots(double r, double eps, int maxIterations) {
-        return findAllRoots(r, eps, maxIterations).stream()
-                .filter(ConvergedRoot::isConverged)
-                .map(ConvergedRoot::getValue)
-                .collect(Collectors.toList());
+        double current = 0.5;
+
+        int currentSkip = 1;
+        do {
+            double previous = current;
+
+            int length = 1;
+            for (; length < currentSkip; length++) {
+                current = f(r, current);
+                if (equals(previous, current, eps)) {
+                    break;
+                }
+            }
+            if (equals(previous, current, eps)) {
+                // cycle possibly found
+                List<Double> result = checkCycle(current, r, length, eps);
+                if (result != null) {
+                    return result;
+                }
+            }
+            currentSkip *= 2;
+        } while (currentSkip <= maxIterations);
+
+        return emptyList();
+    }
+
+    /**
+     * Check that there is a cycle of converged points starting in specified position
+     *
+     * @param start  Start position of cycle
+     * @param r      Parameter
+     * @param length Length of a cycle
+     * @param eps    Epsilon
+     * @return Cycle, if there is some, or null
+     */
+    @Nullable
+    private static List<Double> checkCycle(double start, double r, int length, double eps) {
+        List<Double> result = new ArrayList<>(length);
+        // get cycle
+        for (int i = 0; i < length; i++) {
+            result.add(start);
+            start = f(r, start);
+        }
+        // Check that cycle converged. For do it check that every element X equals to f^length (X)
+        boolean cycleConverged = equals(start, result.get(0), eps);
+
+        for (int i = 1; i < result.size(); i++) {
+            cycleConverged &= equals(result.get(i), f(r, result.get(i - 1)), eps);
+        }
+        if (cycleConverged) {
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    private static boolean equals(double a, double b, double eps) {
+        return abs(a - b) < eps;
     }
 
     /**
      * Find series of points, starting from 0.5 to all roots
      *
-     * @param r Parameter
+     * @param r   Parameter
      * @param eps Epsilon
      * @return Series of points
      */
     @Nonnull
     public static List<Double> findConvergeSeries(double r, double eps, int maxIterations) {
-        return findAllRoots(r, eps, maxIterations).stream()
-                .map(ConvergedRoot::getValue)
-                .collect(Collectors.toList());
-    }
-
-    @Nonnull
-    private static List<ConvergedRoot> findAllRoots(double r, double eps, int maxIterations) {
         double current = 0.5;
-        int iterations = 0;
 
-        List<ConvergedRoot> roots = new ArrayList<>();
-        ConvergedRoot closest = new ConvergedRoot(current, false);
-        roots.add(closest);
-
+        List<Double> result = new ArrayList<>();
+        result.add(current);
+        int currentSkip = 1;
         do {
-            current = f(r, closest.getValue());
+            double previous = current;
 
-            // Find closest root
-            for (ConvergedRoot root : roots) {
-                if (Double.compare(abs(root.value - current), abs(closest.value - current)) < 0) {
-                    closest = root;
+            int length = 1;
+            for (; length < currentSkip; length++) {
+                current = f(r, current);
+                if (equals(previous, current, eps)) {
+                    break;
+                }
+                result.add(current);
+            }
+            if (equals(previous, current, eps)) {
+                // cycle possibly found
+                List<Double> cycle = checkCycle(current, r, length, eps);
+                if (cycle != null) {
+                    result.addAll(cycle);
+                    return result;
                 }
             }
-
-            if (abs(closest.value - current) < eps) {
-                // Root converged
-                closest.converged = true;
-            } else {
-                // Root doesn't converge
-                closest = new ConvergedRoot(current, false);
-                roots.add(closest);
-            }
-
-            // If we found a finite number of roots, then we found a cycle.
-            // If last root in cycle converge, that all roots were converged
-            if (roots.get(roots.size() - 1).isConverged()) {
-                break;
-            }
-        } while (++iterations < maxIterations);
-
-        return roots;
-    }
-
-    /**
-     * Root of equation that can be converged or not
-     */
-    private static class ConvergedRoot {
-        double value;
-
-        boolean converged;
-
-        public ConvergedRoot(double value, boolean converged) {
-            this.value = value;
-            this.converged = converged;
-        }
-
-        public double getValue() {
-            return value;
-        }
-
-        public boolean isConverged() {
-            return converged;
-        }
+            currentSkip *= 2;
+        } while (currentSkip <= maxIterations);
+        return result;
     }
 }
